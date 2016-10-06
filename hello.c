3cec029c2d01b71ac7d373f0cd5fd491e99d12da
Jun-ho Lee <leejunho_@naver.com>
2016-10-06 16:59:02 +0900

    HELLO v1.3b.0 - FOU debugging

--- hello.c
+++ hello.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void fou()
+{
+	printf("2           7     \n");
+	printf("  0       1   3   \n");
+	printf("    1   0       0 \n");
+	printf("      2           \n");
+}
+
 void main()
 {
 	printf("Hello! \n");
